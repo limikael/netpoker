c23b3fb8eae296ece2a950f6fe09c1e895dae234
Mikael Lindqvist <li.mikael@gmail.com>
2014-06-23 00:26:16 +0200

    slowly...

---
+++ src/utils/MessageServer.js
@@ -0,0 +1,22 @@
+/**
+ * Server that manages connections where each message is a JSON document
+ * on one line.
+ * @class MessageServer
+ */
+function MessageServer() {
+
+}
+
+/**
+ * Set security options.
+ * @method setSecurity
+ */
+MessageServer.setSecurity = function(key, cert) {}
+
+/**
+ * Start listening for connections.
+ * @method listen
+ */
+MessageServer.listen = function(port) {
+
+}
