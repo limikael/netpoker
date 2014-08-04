a19ad4810f64d4cae056a36513c0e850174261ae
Mikael Lindqvist <li.mikael@gmail.com>
2014-08-05 00:11:36 +0200

    created server class

--- src/utils/MessageServer.js
+++ src/utils/MessageServer.js
@@ -1,22 +1,33 @@
+var WebSocket = require("faye-websocket");
+var http = require("http");
+
 /**
- * Server that manages connections where each message is a JSON document
- * on one line.
+ * Server that manages connections where each message is a JSON document.
  * @class MessageServer
  */
 function MessageServer() {
+	this.server = http.createServer();
 
+	this.server.on("upgrade", this.onServerUpgrade.bind(this));
+}
+
+MessageServer.prototype.onServerUpgrade = function(request, socket, body) {
+	if (!WebSocket.isWebSocket(request))
+		return;
+
+	var ws = new WebSocket(request, socket, body);
 }
 
 /**
  * Set security options.
  * @method setSecurity
  */
-MessageServer.setSecurity = function(key, cert) {}
+MessageServer.prototype.setSecurity = function(key, cert) {}
 
 /**
  * Start listening for connections.
  * @method listen
  */
-MessageServer.listen = function(port) {
-
+MessageServer.prototype.listen = function(port) {
+	this.server.listen(port);
 }
