0bb8005aa94096f45101ff8563f624b30d5c005b
Mikael Lindqvist <li.mikael@gmail.com>
2014-08-06 22:50:48 +0200

    sequencer

---
+++ src/js/utils/Sequencer.js
@@ -0,0 +1,80 @@
+var FunctionUtil = require("./FunctionUtil");
+var EventDispatcher = require("./EventDispatcher");
+
+/**
+ * Perform tasks in a sequence.
+ * Tasks, which should be event dispatchers,
+ * are euqueued with the enqueue function,
+ * a START event is dispatcher upon task
+ * start, and the task is considered complete
+ * as it dispatches a COMPLETE event.
+ * @class Sequencer
+ */
+function Sequencer() {
+	EventDispatcher.call(this);
+
+	this.queue = [];
+	this.currentTask = null;
+	this.onTaskCompleteClosure = this.onTaskComplete.bind(this);
+}
+
+FunctionUtil.extend(Sequencer, EventDispatcher);
+
+Sequencer.START = "start";
+Sequencer.COMPLETE = "complete";
+
+/**
+ * Enqueue a task to be performed.
+ * @method enqueue
+ */
+Sequencer.prototype.enqueue = function(task) {
+	if (!this.currentTask)
+		this.startTask(task)
+
+	else
+		this.queue.push(task);
+}
+
+/**
+ * Start the task.
+ * @private
+ */
+Sequencer.prototype.startTask = function(task) {
+	this.currentTask = task;
+
+	this.currentTask.addEventListener(Sequencer.COMPLETE, this.onTaskCompleteClosure);
+	this.currentTask.dispatchEvent({
+		type: Sequencer.START
+	});
+}
+
+/**
+ * The current task is complete.
+ * @private
+ */
+Sequencer.prototype.onTaskComplete = function() {
+	this.currentTask.removeEventListener(Sequencer.COMPLETE, this.onTaskCompleteClosure);
+	this.currentTask = null;
+
+	if (this.queue.length > 0)
+		this.startTask(this.queue.shift());
+
+	else
+		this.trigger(Sequencer.COMPLETE);
+
+}
+
+/**
+ * Abort the sequence.
+ * @method abort
+ */
+Sequencer.prototype.abort = function() {
+	if (this.currentTask) {
+		this.currentTask.removeEventListener(Sequencer.COMPLETE, this.onTaskCompleteClosure);
+		this.currentTask = null;
+	}
+
+	this.queue = [];
+}
+
+module.exports = Sequencer;
---
+++ test/js.unit/utils/Sequencer.spec.js
@@ -0,0 +1,33 @@
+var Sequencer = require("../../../src/js/utils/Sequencer");
+var EventDispatcher = require("../../../src/js/utils/EventDispatcher");
+
+describe("Sequencer", function() {
+	it("sequences stuff", function() {
+		var s = new Sequencer();
+		var item1=new EventDispatcher();
+		var item2=new EventDispatcher();
+
+		var item1start=jasmine.createSpy();
+		var item2start=jasmine.createSpy();
+		var complete=jasmine.createSpy();
+
+		item1.on(Sequencer.START,item1start);
+		item2.on(Sequencer.START,item2start);
+		s.on(Sequencer.COMPLETE,complete);
+
+		s.enqueue(item1);
+		s.enqueue(item2);
+
+		expect(item1start).toHaveBeenCalled();
+		expect(item2start).not.toHaveBeenCalled();
+
+		item1.trigger(Sequencer.COMPLETE);
+
+		expect(item2start).toHaveBeenCalled();
+		expect(complete).not.toHaveBeenCalled();
+
+		item2.trigger(Sequencer.COMPLETE);
+
+		expect(complete).toHaveBeenCalled();
+	});
+});
