53720d838f36140b4c5a159c715b77423e05408a
Mikael Lindqvist <li.mikael@gmail.com>
2014-08-31 11:47:27 +0200

    nine slice

---
+++ src/js/utils/NineSlice.js
@@ -0,0 +1,110 @@
+var PIXI = require("pixi.js");
+var FunctionUtil = require("./FunctionUtil");
+
+/**
+ * Nine slice.
+ */
+function NineSlice(texture, left, top, right, bottom) {
+	PIXI.DisplayObjectContainer.call(this);
+
+	this.texture = texture;
+
+	if (!top)
+		top = left;
+
+	if (!right)
+		right = left;
+
+	if (!bottom)
+		bottom = top;
+
+	this.left = left;
+	this.top = top;
+	this.right = right;
+	this.bottom = bottom;
+
+	this.localWidth = texture.width;
+	this.localHeight = texture.height;
+
+	this.buildParts();
+	this.updateSizes();
+}
+
+FunctionUtil.extend(NineSlice, PIXI.DisplayObjectContainer);
+
+/**
+ * Build parts.
+ */
+NineSlice.prototype.buildParts = function() {
+	var xp = [0, this.left, this.texture.width - this.right, this.texture.width];
+	var yp = [0, this.top, this.texture.height - this.bottom, this.texture.height];
+	var hi, vi;
+
+	this.parts = [];
+
+	for (vi = 0; vi < 3; vi++) {
+		for (hi = 0; hi < 3; hi++) {
+			var w = xp[hi + 1] - xp[hi];
+			var h = yp[vi + 1] - yp[vi];
+
+			if (w != 0 && h != 0) {
+				var texturePart = this.createTexturePart(xp[hi], yp[vi], w, h);
+				var s = new PIXI.Sprite(texturePart);
+				this.addChild(s);
+
+				this.parts.push(s);
+			} else {
+				this.parts.push(null);
+			}
+		}
+	}
+}
+
+/**
+ * Update sizes.
+ */
+NineSlice.prototype.updateSizes = function() {
+	var xp = [0, this.left, this.localWidth - this.right, this.localWidth];
+	var yp = [0, this.top, this.localHeight - this.bottom, this.localHeight];
+	var hi, vi, i = 0;
+
+	for (vi = 0; vi < 3; vi++) {
+		for (hi = 0; hi < 3; hi++) {
+			if (this.parts[i]) {
+				var part = this.parts[i];
+
+				part.position.x = xp[hi];
+				part.position.y = yp[vi];
+				part.width = xp[hi + 1] - xp[hi];
+				part.height = yp[vi + 1] - yp[vi];
+			}
+
+			i++;
+		}
+	}
+}
+
+/**
+ * Set local size.
+ */
+NineSlice.prototype.setLocalSize = function(w, h) {
+	this.localWidth = w;
+	this.localHeight = h;
+	this.updateSizes();
+}
+
+/**
+ * Create texture part.
+ */
+NineSlice.prototype.createTexturePart = function(x, y, width, height) {
+	var frame = {
+		x: this.texture.frame.x + x,
+		y: this.texture.frame.y + y,
+		width: width,
+		height: height
+	};
+
+	return new PIXI.Texture(this.texture, frame);
+}
+
+module.exports = NineSlice;
