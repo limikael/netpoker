99c412137f8c0ce58464ff396a5da6e834797737
Mikael Lindqvist <li.mikael@gmail.com>
2014-10-06 16:42:01 +0200

    command line

--- src/js/server/netpokerserver.js
+++ src/js/server/netpokerserver.js
@@ -2,17 +2,45 @@
 
 var minimist = require("minimist");
 var NetPokerServer = require("./app/NetPokerServer");
-var MockBackendServer = require("./backend/MockBackendServer");
-var Backend = require("./backend/Backend");
+var MockBackendServer = require("./mock/MockBackendServer");
+var MockWebRequestHandler = require("./mock/MockWebRequestHandler");
+
+function usage() {
+	console.log("Usage: netpokerserver <options>");
+	console.log("");
+	console.log("Clientport and either backend or mock needs to be specified.");
+	console.log("");
+	console.log("Options:");
+	console.log("");
+	console.log("  --clientport <port>  Port where to listen for incoming connections.");
+	console.log("  --backend <url>      Which backend to connect to.");
+	console.log("  --mock               Start a mocked server without backend.");
+	console.log("");
+
+	process.exit(1);
+}
 
 var args = minimist(process.argv.slice(2));
 
 var netPokerServer = new NetPokerServer();
 
+if (args["backend"]) {
+	netPokerServer.setBackendUrl(args["backend"]);
+} else if (args["mock"]) {
+	netPokerServer.serveViewCases(__dirname + "/../../../res/viewcases");
+	netPokerServer.setBackend(new MockBackendServer());
+	netPokerServer.setWebRequestHandler(new MockWebRequestHandler());
+} else {
+	usage();
+}
+
+if (!args["clientport"])
+	usage();
+
 netPokerServer.setListenPort(args["clientport"]);
 
-var backend = new Backend("http://localhost:9999");
-
-netPokerServer.run();
-
-console.log(args);
+netPokerServer.run().then(
+	function() {
+		console.log("==== SERVER LISTENING ON PORT: " + args["clientport"] + " ====");
+	}
+);
