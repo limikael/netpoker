e9dde3ba7855b75223144f9cbe23250a901fbd5e
Mikael Lindqvist <li.mikael@gmail.com>
2014-10-06 15:48:27 +0200

    mock web request handler

---
+++ src/js/server/mock/MockWebRequestHandler.js
@@ -0,0 +1,52 @@
+var fs = require("fs");
+var url = require("url");
+
+/**
+ * Handle a web request when in mock mode.
+ * @class MockWebRequestHandler
+ */
+function MockWebRequestHandler() {}
+
+/**
+ * Handle a web request.
+ * @method handleWebRequest
+ */
+MockWebRequestHandler.prototype.handleWebRequest = function(e) {
+	console.log("web request: " + e.request.url);
+
+	var urlParts = url.parse(e.request.url);
+	var path = urlParts.pathname;
+
+	var fileName = __dirname + "/../../../../res/mocksite/" + path;
+
+	if (path == "/") {
+		var caseListContent = "";
+		var dir = fs.readdirSync(__dirname + "/../../../../res/viewcases");
+		for (var i = 0; i < dir.length; i++) {
+			var viewCase = dir[i].replace(".json", "");
+
+			caseListContent += "<li>";
+			caseListContent += "<a href='table.html?viewcase=" + viewCase + "'>" + viewCase + "</a>";
+			caseListContent += "</li>"
+		}
+
+		var content = fs.readFileSync(__dirname + "/../../../../res/mocksite/index.html");
+		content = content.toString().replace("{caseListContent}", caseListContent);
+
+		e.response.write(content);
+	} else if (fs.existsSync(fileName)) {
+		e.response.write(fs.readFileSync(fileName));
+	}
+
+	e.response.end();
+}
+
+/**
+ * Attach this MockWebRequestHandler to a NetPokerServer
+ * @method attachToServer
+ */
+MockWebRequestHandler.prototype.attachToServer = function(server) {
+	server.on("request", this.handleWebRequest, this);
+}
+
+module.exports = MockWebRequestHandler;
--- test/tools/mockserver.js
+++ test/tools/mockserver.js
@@ -3,58 +3,25 @@
 var NetPokerServer = require("../../src/js/server/app/NetPokerServer");
 var Backend = require("../../src/js/server/backend/Backend");
 var MockBackendServer = require("../../src/js/server/mock/MockBackendServer");
+var MockWebRequestHandler = require("../../src/js/server/mock/MockWebRequestHandler");
 var minimist = require("minimist");
 var fs = require("fs");
 var url = require("url");
 
-function handleWebRequest(e) {
-	console.log("web request: " + e.request.url);
-
-	var urlParts = url.parse(e.request.url);
-	var path = urlParts.pathname;
-
-	var fileName = __dirname + "/../../res/mocksite/" + path;
-
-	if (path == "/") {
-		var caseListContent = "";
-		var dir = fs.readdirSync(__dirname + "/../../res/viewcases");
-		for (var i = 0; i < dir.length; i++) {
-			var viewCase = dir[i].replace(".json", "");
-
-			caseListContent += "<li>";
-			caseListContent += "<a href='table.html?viewcase=" + viewCase + "'>" + viewCase + "</a>";
-			caseListContent += "</li>"
-		}
-
-		var content = fs.readFileSync(__dirname + "/../../res/mocksite/index.html");
-		content = content.toString().replace("{caseListContent}", caseListContent);
-
-		e.response.write(content);
-	} else if (fs.existsSync(fileName)) {
-		e.response.write(fs.readFileSync(fileName));
-	}
-
-	e.response.end();
-}
-
 var port = 2222;
-var mockBackendPort = 9999;
-
-var mockBackendServer = new MockBackendServer();
-mockBackendServer.setListenPort(mockBackendPort);
 
 var netPokerServer = new NetPokerServer();
 netPokerServer.setListenPort(port);
 netPokerServer.serveViewCases(__dirname + "/../../res/viewcases");
-netPokerServer.setBackend(new Backend("http://localhost:" + mockBackendPort));
-netPokerServer.on("request", handleWebRequest);
+netPokerServer.setBackend(new MockBackendServer());
 
-mockBackendServer.start();
+var webRequestHandler = new MockWebRequestHandler();
+webRequestHandler.attachToServer(netPokerServer);
+
 netPokerServer.listen();
-
-//setTimeout(function() {
-	netPokerServer.run().then(function() {
+netPokerServer.run().then(
+	function() {
 		console.log("");
 		console.log("Mockserver started on port " + port + ", visit: http://localhost:2222/");
-	});
-//}, 1000);
+	}
+);
