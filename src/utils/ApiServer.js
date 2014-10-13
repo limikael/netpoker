6857d996231064bd74c8563fac5a7bc6c732262f
Mikael Lindqvist <li.mikael@gmail.com>
2014-10-13 15:51:34 +0200

    api server

--- src/utils/ApiServer.js
+++ src/utils/ApiServer.js
@@ -7,7 +7,90 @@ var url = require("url");
  * @class ApiServer
  */
 function ApiServer() {
+	this.handlers = {};
+}
 
+/**
+ * Register handler.
+ * @method registerHandler
+ */
+ApiServer.prototype.registerHandler = function(func, handler) {
+	this.handlers[func] = handler;
+}
+
+/**
+ * Handle a method.
+ * @method handleMethod
+ */
+ApiServer.prototype.handleMethod = function(method, parameters) {
+	if (this.handlers[method]) {
+		return this.handlers[method](parameters);
+	}
+}
+
+/**
+ * Can we handle this method?
+ * @method canHandleMethod
+ */
+ApiServer.prototype.canHandleMethod = function(method) {
+	if (this.handlers[method])
+		return true;
+
+	return false;
+}
+
+/**
+ * Can we handle this request?
+ * @method canHandleRequest
+ */
+ApiServer.prototype.canHandleRequest = function(request) {
+	return this.canHandleMethod(this.getMethodByRequest(request));
+}
+
+/**
+ * Map request to api method.
+ * @method getMethodByRequest
+ * @private
+ */
+ApiServer.prototype.getMethodByRequest = function(request) {
+	var urlParts = url.parse(request.url, true);
+
+	var method = urlParts.pathname;
+	method = method.replace("/", "");
+
+	return method;
+}
+
+/**
+ * Handle a web request.
+ * @method handleWebRequest
+ */
+ApiServer.prototype.handleWebRequest = function(request, response) {
+	var method = this.getMethodByRequest(request);
+	var urlParts = url.parse(request.url, true);
+
+	if (!this.canHandleMethod(method)) {
+		response.statusCode = 404;
+		response.write(JSON.stringify("No such method."));
+	} else {
+		try {
+			var result = this.handleMethod(method, urlParts.query);
+			response.write(JSON.stringify(result));
+		} catch (e) {
+			var message;
+
+			if (e instanceof Error)
+				message = e.message;
+
+			else
+				message = e.toString();
+
+			response.statusCode = 500;
+			response.write(JSON.stringify(message));
+		}
+	}
+
+	response.end();
 }
 
 /**
@@ -15,21 +98,7 @@ function ApiServer() {
  * @method onRequest
  */
 ApiServer.prototype.onRequest = function(request, response) {
-	var urlParts = url.parse(request.url, true);
-
-	var method = urlParts.pathname;
-	method = method.replace("/", "");
-
-	//var result = this.handleMethod(method, urlParts.query);
-
-	result = {
-		"hello": "world"
-	};
-
-	if (result)
-		response.write(JSON.stringify(result));
-
-	response.end();
+	this.handleWebRequest(request, response);
 }
 
 /**
--- test/unit/utils/ApiServer.spec.js
+++ test/unit/utils/ApiServer.spec.js
@@ -6,11 +6,48 @@ describe("ApiServer", function() {
 		var apiServer = new ApiServer();
 		apiServer.listen(9999);
 
-		request("http://localhost:9999/testfunc", function(error, response, body) {
+		apiServer.registerHandler("testf", function(p) {
+			return {
+				"hello": "world" + p.val
+			}
+		});
+
+		request("http://localhost:9999/testf?val=2", function(error, response, body) {
+			expect(response.statusCode).toEqual(200);
+
 			expect(JSON.parse(body)).toEqual({
-				"hello": "world"
+				"hello": "world2"
 			});
 
+			apiServer.close();
+			done();
+		});
+	});
+
+	it("handles missing methods gracefully", function(done) {
+		var apiServer = new ApiServer();
+		apiServer.listen(9999);
+
+		request("http://localhost:9999/testfsds", function(error, response, body) {
+			expect(response.statusCode).toEqual(404);
+			expect(JSON.parse(body)).toEqual("No such method.");
+			apiServer.close();
+			done();
+		});
+	});
+
+	it("handles method error gracefully", function(done) {
+		var apiServer = new ApiServer();
+		apiServer.listen(9999);
+
+		apiServer.registerHandler("testf", function() {
+			throw new Error("hello world");
+			//throw "hello world";
+		});
+
+		request("http://localhost:9999/testf", function(error, response, body) {
+			expect(response.statusCode).toEqual(500);
+			expect(JSON.parse(body)).toEqual("hello world");
 			apiServer.close();
 			done();
 		});
