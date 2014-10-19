fc2091391457a0ddba795c033651a6657ffa1a53
micke <micke@micke-X360.(none)>
2014-10-19 21:38:24 +0200

    pollNumPlayers test

--- test/unit/server/api/PollNumPlayersRequestHandler.spec.js
+++ test/unit/server/api/PollNumPlayersRequestHandler.spec.js
@@ -5,22 +5,53 @@ describe("PollNumPlayersRequestHandler", function() {
 	var mockRequest;
 	var mockResponse;
 	var mockCashGameManager;
+	var mockTables;
 
 	beforeEach(function() {
+		mockTables = [];
+
+		for (var i = 0; i < 2; i++) {
+			var t = {};
+			t.id = "table_" + i;
+			t.numInGame = 1;
+
+			t.getId = function() {
+				return this.id;
+			}
+
+			t.getNumInGame = function() {
+				return this.numInGame;
+			}
+
+			mockTables.push(t);
+		}
+
 		mockCashGameManager = {};
+		mockCashGameManager.getTables = function() {
+			return mockTables;
+		}
 
 		mockNetPokerServer = {};
-		mockNetPokerServer.getCashGameManager=function() {
+		mockNetPokerServer.getCashGameManager = function() {
 			return mockCashGameManager;
 		};
 
 		mockRequest = {};
-
 		mockResponse = {};
+		mockResponse.written = "";
+		mockResponse.write = function(s) {
+			this.written += s;
+		}
+		mockResponse.end = jasmine.createSpy();
 	});
 
 	it("returns the number of users if queried without parameters", function() {
-		/*var handler = new PollNumPlayersRequestHandler(mockNetPokerServer);
-		handler.handleRequest(mockRequest, mockResponse);*/
+		var handler = new PollNumPlayersRequestHandler(mockNetPokerServer);
+		handler.handleRequest(mockRequest, mockResponse);
+
+		console.log("written: " + mockResponse.written);
+
+		expect(mockResponse.written).toEqual('{"table_0":1,"table_1":1}');
+		expect(mockResponse.end).toHaveBeenCalled();
 	});
 });
